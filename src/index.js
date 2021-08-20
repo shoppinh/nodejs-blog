85a660dc5b1f366ae92ffbc29e0e6ba99e3eafb6
shoppinh <mactrungkien2000@gmail.com>
2021-08-20 22:34:26 +0700

    from kien

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
+//Body middleware
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 //HTTP logger
 
 app.use(morgan("combined"));
@@ -32,5 +39,9 @@ app.get("/", (req, res) => {
 app.get("/search", (req, res) => {
   console.log(req.query);
   res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 app.listen(port, () => console.log(`Example app listen on ${port}`));
