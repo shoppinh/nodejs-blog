e232da7362bf15f83eb06c55b3b7929bfa79ea26
shoppinh <mactrungkien2000@gmail.com>
2021-08-27 17:29:28 +0700

    .

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,15 @@
+const Course = require("../models/Course");
+
 class SiteController {
   //Home
-  index(req, res) {
-    res.render("home");
+  async index(req, res) {
+    try {
+      const courses = await Course.find();
+      console.log(courses);
+      res.json(courses);
+    } catch (err) {
+      next(err);
+    }
   }
   //Search
   search(req, res) {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 const route = require("./routes");
+const db = require("./config/db/");
 
+//Connect db
+db.connect();
 app.use(express.static(path.join(__dirname, "public")));
 
 //Body middleware
@@ -27,7 +30,7 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources\\views"));
+app.set("views", path.join(__dirname, "resources/views"));
 //route init
 route(app);
 app.listen(port, () => console.log(`Example app listen on ${port}`));
